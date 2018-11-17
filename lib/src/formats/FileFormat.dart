3e8aba186dfea456351b26fb0cdf235555266cdc
TTFTCUTS <emperorlexx@hotmail.com>
2018-11-17 05:03:09 +0000

    Actually, let's make the filename a Generator

--- lib/src/formats/FileFormat.dart
+++ lib/src/formats/FileFormat.dart
@@ -72,12 +72,14 @@ abstract class FileFormat<T,U> {
         return container;
     }
 
-    static Element saveButton<T,U>(FileFormat<T,U> format, Generator<T> objectGetter, {String caption = "Save file", String filename = "download"}) {
+    static String _defaultFilename() => "download";
+
+    static Element saveButton<T,U>(FileFormat<T,U> format, Generator<T> objectGetter, {String caption = "Save file", Generator<String> filename = _defaultFilename}) {
         Element container = new DivElement();
 
         ButtonElement download = new ButtonElement()..text=caption;
 
-        AnchorElement link = new AnchorElement()..style.display="none"..download=filename;
+        AnchorElement link = new AnchorElement()..style.display="none"..download=filename();
 
         download..onClick.listen((Event e) async {
             T object = objectGetter();
